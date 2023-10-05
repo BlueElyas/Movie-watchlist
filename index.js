4973c0531560b17481900d0dc79effd2eca295a1
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-10-05 19:25:49 +0100

    add cursor styles to index.html

---
+++ index.js
@@ -0,0 +1,10 @@
+// Take the input of the value when the button is clicked
+// Put it in the promise of API to search it up in a template literal
+// When the icon is clicked, add to local storage
+
+// If it works, save to local storage
+// If not, display error message
+
+
+// When on watchlist, take any items from local storage and display on screen
+// If an item is removed, remove from local storage and unrender
