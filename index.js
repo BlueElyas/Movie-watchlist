400e01bfb09c4fdc725aac9b4c85f9ca161254aa
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-10-06 00:18:56 +0100

    render movies on the page
    i fixed the API to grab all movies that have the name in their title, next is storing whichever is added in local storage

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 // If it works, save to local storage
 // If not, display error message
 
+let arrayOfMovies = []
 
 // When on watchlist, take any items from local storage and display on screen
 // If an item is removed, remove from local storage and unrender
@@ -14,15 +15,47 @@
 const searchInput = document.getElementById('search-input')
 const searchBtn = document.getElementById('submit-search')
 
+const movieSearchArea = document.getElementById('movie-search-area')
+
 //RENDER RESULTS WITH s=? THEN RENDER EACH ONE WITH A FOR LOOP
 
+
 searchBtn.addEventListener('click', () => {
-    let inputValue = searchInput.value
+    const inputValue = searchInput.value
     fetch(`http://www.omdbapi.com/?apikey=15e9726&s=${inputValue}`, {method: "GET"})
     .then(res => res.json())
     .then(data => {
-        data.Search.map(movies => console.log(movies.Title))
+        renderItems(data.Search)
     })
 })
+
+function renderItems(array) {
+    for (let movie of array) {
+        fetch(`http://www.omdbapi.com/?apikey=15e9726&t=${movie.Title}`, {method: "GET"})
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                movieSearchArea.innerHTML += `
+                <div class="movie-container" id="movie-container">
+                    <img src="${data.Poster}" alt="">
+                    <div class="movie-content">
+                        <div class="movie-header">
+                            <h2>${data.Title}</h2>
+                            <i class="fa-solid fa-star" style="color: #fec654;"></i>
+                            <p>${data.imdbRating}</p>
+                        </div>
+                        <div class="movie-details" id="movie-details">
+                            <p>${data.Runtime}</p>
+                            <p>${data.Genre}</p>
+                            <div class="watchlist-button">
+                                <i class="fa-solid fa-circle-plus"></i>Watchlist</div>
+                        </div>
+                        <p>${data.Plot}</p>
+                    </div>
+                </div>
+                `
+            })
+    }
+}
 
 // localStorage.setItem(inputValue, "test")
