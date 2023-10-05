4f47e063cd47e47172277555191c17500f6e573a
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-10-05 19:42:31 +0100

    test the input and button
    it is console logging the input using an event listener and also checking if local storage works

--- index.js
+++ index.js
@@ -2,9 +2,23 @@
 // Put it in the promise of API to search it up in a template literal
 // When the icon is clicked, add to local storage
 
+// If movie does not exist, do not push into localstorage
+
 // If it works, save to local storage
 // If not, display error message
 
 
 // When on watchlist, take any items from local storage and display on screen
 // If an item is removed, remove from local storage and unrender
+
+const searchInput = document.getElementById('search-input')
+const searchBtn = document.getElementById('submit-search')
+
+
+
+searchBtn.addEventListener('click', () => {
+    let inputValue = searchInput.value
+    console.log(inputValue)
+    localStorage.setItem(inputValue, "test")
+})
+
