8cc9f0e359f3ab70d620d5728708605867667253
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-10-07 20:59:21 +0100

    create the saved movies in the watchlist

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-// Take the input of the value when the button is clicked
-// Put it in the promise of API to search it up in a template literal
-// When the icon is clicked, add to local storage
-
-// If movie does not exist, do not push into localstorage
-
-// If it works, save to local storage
-// If not, display error message
-
-// When on watchlist, take any items from local storage and display on screen
-// If an item is removed, remove from local storage and unrender
-
 const searchInput = document.getElementById('search-input')
 const searchBtn = document.getElementById('submit-search')
-
 const movieSearchArea = document.getElementById('movie-search-area')
+
+
 
 //RENDER RESULTS WITH s=? THEN RENDER EACH ONE WITH A FOR LOOP
 
+window.addEventListener('DOMContentLoaded', () => {
+})
 
 searchBtn.addEventListener('click', () => {
     const inputValue = searchInput.value
@@ -53,20 +44,61 @@ function renderItems(array) {
                 </div>
                 `
 
-                movieSearchArea.appendChild(movieContainer);
+                movieSearchArea.appendChild(movieContainer)
                 const addToListIcon = movieContainer.querySelector('.add-to-list')
 
                 addToListIcon.addEventListener('click', () => {
                     const movieTitle = addToListIcon.getAttribute('data-title')
                     localStorage.setItem(movieTitle, movieTitle)
+                    console.log(movieTitle)
+                    renderWatchlist(movieTitle)
                 })
             })
     }
 }
 
 
-function renderWatchlist() {
+function renderWatchlist(movie) {
+    const watchListArea = document.getElementById('watchlist-area')
+    const movieWatch = localStorage.getItem(movie)
+    console.log(movieWatch)
+    fetch(`http://www.omdbapi.com/?apikey=15e9726&t=${movieWatch}`, { method: "GET" })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        const watchlistContainer = document.createElement('div')
+        watchlistContainer.classList.add('movie-container')
+        watchlistContainer.innerHTML =
+        `<img src="${data.Poster}" alt="">
+        <div class="movie-content">
+            <div class="movie-header">
+                <h2>${data.Title}</h2>
+                <i class="fa-solid fa-star" style="color: #fec654;"></i>
+                <p>${data.imdbRating}</p>
+            </div>
+            <div class="movie-details" id="movie-details">
+                <p>${data.Runtime}</p>
+                <p>${data.Genre}</p>
+                <div class="watchlist-button">
+                    <i class="fa-solid fa-circle-minus remove-from-list"></i>Watchlist
+                </div>
+            </div>
+            <p>${data.Plot}</p>
+        </div>`
+        console.log(data.Genre)
 
+
+        watchListArea.appendChild(watchlistContainer)
+        const removeFromListIcon = watchlistContainer.querySelector('.remove-from-list')
+
+
+        removeFromListIcon.addEventListener('click', () => {
+            const movieTitle = removeFromListIcon.getAttribute('data-title')
+            localStorage.removeItem(movieTitle, movieTitle)
+        })
+    }) .catch(error => {
+        console.error("Error:", error)
+    })
 }
 
 
